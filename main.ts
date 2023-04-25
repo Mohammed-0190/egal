fd59f4b5bbf41cbe1265cf49b91e4a0fc9ebc033
Mohammed-0190 <131755029+Mohammed-0190@users.noreply.github.com>
2023-04-25 10:37:19 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 basic.forever(function () {
-	
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.setLedColor(0x00ff00)
+        basic.showIcon(IconNames.Yes)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.setLedColor(0xff0000)
+        basic.showIcon(IconNames.No)
+    }
 })
